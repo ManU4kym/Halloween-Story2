6574f39416a4059aaa7980bd47c90be18988f816
ManU4kym <emmannjoroge36@gmail.com>
2024-05-29 09:54:43 +0300

    bush mesh add to house

--- src/script.js
+++ src/script.js
@@ -61,9 +61,34 @@ const door = new THREE.Mesh(
 )
 
 door.position.y = 1
-door.position.z = 2
+door.position.z = 2.01 //z-fighting // Glitch effect
 house.add(door)
 
+/* Bushe */
+
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial()
+/* bush mesh */
+const bush1 = new THREE.Mesh(bushGeometry,bushMaterial)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+const bush2 = new THREE.Mesh(bushGeometry,bushMaterial)
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry,bushMaterial)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(-0.8, 0.1, 2.2)
+
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.15,0.15, 0.15)
+bush4.position.set(-1, 0.05,2.6)
+
+
+
+house.add(bush1, bush2,bush3,bush4)
 /**
  * Lights
  */
